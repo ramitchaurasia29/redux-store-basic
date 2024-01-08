1811267e670286a9595323c568ce51837cd4709d
ramit <ramit2900@gmail.com>
2024-01-08 12:37:43 +0530

    7 commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './containers/Header';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ProductListing from './containers/ProductListing';
+import ProductDetails from './containers/ProductDetails';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path='/' exact element={<ProductListing />} />
+          <Route path='/product/:productId' exact element={<ProductDetails />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
